890542eb16c95056394f683dfe95dd673031be43
Leonardo Vedovate <lvedovate@outlook.com>
2022-01-07 20:22:46 -0300

    first commit

---
+++ EstudoDeExtensions/Extensions/StringExensions.cs
@@ -0,0 +1,17 @@
+﻿namespace System
+{
+    internal static class StringExensions
+    {
+        public static string Cut(this string thisObj, int tam)
+        {
+            if (thisObj.Length <= tam)
+            {
+                return thisObj;
+            }
+            else
+            {
+                return thisObj.Substring(0, tam) + "...";
+            }
+        }
+    }
+}
---
+++ EstudoDeExtensions/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+string s1 = "Good morning dear studants!";
+
+Console.WriteLine(s1.Cut(10));
